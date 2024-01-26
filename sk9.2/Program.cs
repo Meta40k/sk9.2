6c2e3a74d3211dbbd23cb9e0996ec208c940e7ef
Denis Lopatkin <56197693+Meta40k@users.noreply.github.com>
2024-01-26 15:16:27 +0300

    sk9.2 закрыл и выложил в гит

---
+++ sk9.2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace sk92
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> LastNames = new List<string>
+            {
+                "Иванов",
+                "Петров",
+                "Сидоров",
+                "Лопаткин",
+                "Метак"
+            };
+
+            UserService userService = new UserService();
+            userService.UserNumberReaderEvent += Sorting;
+            userService.ReadUserNumber(LastNames);
+        }
+
+        static void Sorting(int number, List<string> LastNames)
+        {
+            switch (number)
+            {
+                case 1: SortLastNames(LastNames); break;
+                case 2: SortLastNamesDesc(LastNames); break;
+            }
+        }
+
+        static void SortLastNamesDesc(List<string> LastNames)
+        {
+            var SortedLastNames = LastNames.OrderByDescending(x => x);
+            foreach (string lastname in SortedLastNames)
+            { 
+                Console.WriteLine(lastname);
+            }
+        }
+
+        static void SortLastNames(List<string> LastNames)
+        {
+            var SortedLastNames = LastNames.OrderBy(x => x);
+            foreach (string lastname in SortedLastNames)
+            {
+                Console.WriteLine(lastname);
+            }
+        }
+    }
+}
---
+++ sk9.2/userService.cs
@@ -0,0 +1,37 @@
+namespace sk92
+{
+    public class UserService
+    {
+        public delegate void UserNumberReaderDelegate(int number, List<string> lastNames);
+        public event UserNumberReaderDelegate UserNumberReaderEvent;
+
+        public void ReadUserNumber(List<string> LastNames)
+        {
+            Console.WriteLine("Введите метод сортировки: 1 - по алфавиту, 2 - в обратном порядке:");
+            int InputNumber = 0;
+            bool IsCorrect = true;
+            while (IsCorrect) 
+            { 
+                try
+                {
+                    InputNumber = Convert.ToInt32(Console.ReadLine());
+                    if (InputNumber != 1 && InputNumber != 2)
+                    {
+                        throw new FormatException();
+                    }
+                    IsCorrect = false;
+                }
+                catch (FormatException) 
+                {
+                    Console.WriteLine("Введенено некорректное значение. Введите 1 или 2:");
+                }
+            }
+            UserNumberReader(InputNumber, LastNames);
+        }
+
+        protected virtual void UserNumberReader(int number, List<string> lastNames) 
+        {
+            UserNumberReaderEvent?.Invoke(number, lastNames);
+        }
+    }
+}
